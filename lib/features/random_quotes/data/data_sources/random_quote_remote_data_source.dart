542d365f9ab19d2d0eb6a23baaa4e66dc286d68d
LAPTOP SHOP <aminahamed672000@gmail.com>
2023-10-30 11:54:01 +0200

    +remoteDataSource

--- lib/features/random_quotes/data/data_sources/random_quote_remote_data_source.dart
+++ lib/features/random_quotes/data/data_sources/random_quote_remote_data_source.dart
@@ -1,5 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:quotes_app/core/api/end_points.dart';
+import 'package:quotes_app/core/errors/exceptions.dart';
+
 import '../models/QuoteModel.dart';
 
 abstract class RandomQuoteRemoteDataSource {
   Future<QuoteModel> getRandomQuote();
+}
+
+class RandomQuoteRemoteDataSourceImpl implements RandomQuoteRemoteDataSource {
+  final http.Client client;
+
+  RandomQuoteRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<QuoteModel> getRandomQuote() async {
+    final http.Response response =
+        await client.get(Uri.parse(EndPoints.randomQuote));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        return QuoteModel.fromJson(data[0]);
+      }
+    }
+    throw ServerException();
+  }
 }
